443a932fad2e6e69ea794dedf779f1369df646da
Do Nhat Minh <mrordinaire@gmail.com>
2013-03-29 15:33:30 +0800

    move html outside of source

--- simplechat/chat.go
+++ simplechat/chat.go
@@ -2,8 +2,10 @@ package main
 
 import (
     "html/template"
+    "io/ioutil"
     "log"
     "net/http"
+    "os"
 
     "code.google.com/p/go.net/websocket"
 )
@@ -14,91 +16,27 @@ type message struct {
 }
 
 var (
-    page = template.Must(template.New("root").Parse(`
-<!doctype html>
-<html>
-    <head>
-        <title>Simple chat</title>
-        <style>
-            #frame {
-                text-align: center;
-            }
-
-            #logo {
-                text-transform: uppercase;
-                margin: auto;
-            }
-        </style>
-    </head>
-    <body>
-        <div id="frame">
-            <div id="logo">Simple chat</div>
-            <div id="lines"></div>
-            <input id="name" name="line" type="textarea" />
-            <input id="line" name="line" type="textarea" />
-            <input id="send" value="Send" type="submit" />
-        </div>
-        <script>
-            var socket = new WebSocket("ws://{{.}}/socket")
-            socket.onmessage = function(m) {
-                var message = JSON.parse(m.data)
-                var lines = document.getElementById("lines")
-                var line = document.createElement("div")
-                var user = document.createElement("span")
-                var content = document.createElement("span")
-                var br = document.createElement("br")
-
-                user.class = "user"
-                if (message.u) {
-                    user.textContent = message.u + ":"
-                } else {
-                    user.textContent = "anonymous:"
-                }
-
-                content.class = "content"
-                content.textContent = message.m
-
-                line.appendChild(user)
-                line.appendChild(content)
-                line.appendChild(br)
-
-                lines.appendChild(line)
-            }
-
-            var button = document.getElementById("send")
-            button.onclick = function () {
-                var line = document.getElementById("line")
-                var name = document.getElementById("name")
-                if (line.value) {
-                    socket.send(JSON.stringify({
-                        u: name.value,
-                        m: line.value
-                    }))
-                    line.value = ""
-                }
-            }
-
-            var text = document.getElementById("line")
-            text.onkeyup = function(e) {
-                if ((e.keyCode || e.which) == 13) {
-                    button.onclick()
-                    return false
-                }
-            }
-        </script>
-    </body>
-</html>
-    `))
-
     listenAddress = "localhost:4000"
-
+    page *template.Template
     list broadcastList
 )
 
 func main() {
+    file, err := os.Open("chat.html")
+    if err != nil {
+        log.Fatalln(err)
+    }
+    rawBytes, err := ioutil.ReadAll(file)
+    if err != nil {
+        log.Fatalln(err)
+    }
+    file.Close()
+
+    page = template.Must(template.New("root").Parse(string(rawBytes)))
+
     http.HandleFunc("/", serve)
     http.Handle("/socket", websocket.Handler(chat))
-    err := http.ListenAndServe(listenAddress, nil)
+    err = http.ListenAndServe(listenAddress, nil)
     if err != nil {
         log.Fatalln(err)
     }
